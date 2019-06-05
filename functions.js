d6ba5d720b6632cbd8f339a3dc18cdae83eb9cdb
eva <littlespringrollfun@gmail.com>
2019-06-04 20:35:57 -0400

    simple page

--- functions.js
+++ functions.js
@@ -2,48 +2,119 @@ var sample_variable = "VARIABLES";
 
 var functions = {
 
-sample_function: {
-  description: `<p>description can have <a href="#test_function">links</a></p>`,
-  syntax: 'syntax()',
-  args: [
-    'argument', 'description',
-    'argument', 'description'
-  ],
-  returns: 'return value',
-  example: {
-    code:
-`the code can't have indentation to the normal level
-because it needs to use pre
-    yeet`,
-    description: `
-      <p>Use backtick to multiline string (template technically)</p>
-      <p>You can include ${sample_variable} in it hehe</p>`
-  }
-}, // remember to put a comma between functions
+create_melee: {
+	description:
+		`<p>This function creates a new instance of <a href="#obj_melee">obj_melee</a> and initializes it with the arguments you provided.</p>
+		<p>A melee hitbox is an attached hitbox that is tied to the owner's hitbox groups. It is the basis for most attacks.</p>`,
+	syntax: 'create_melee(rel_x, rel_y, xscale, yscale, damage, knockback, scaling, hitlag, angle, lifetime, shape, group, [flipper]);',
+	args: [
+		'rel_x', 'The x offset that the hitbox will be created at. It is automatically flipped based on the direction the player is facing.',
+		'rel_y', 'The y offset that the hitbox will be created at',
+		'xscale', 'The xscale of the hitbox object. The base sprites are 64px wide',
+		'yscale', 'The yscale of the hitbox object. The base sprites are 64px tall',
+		'damage', 'The amount of damage the hitbox does',
+		'knockback', 'The base knockback of the hitbox',
+		'scaling', 'The number used for knockback calculation. A higher number means the move has more knockback with higher damage',
+		'hitlag', 'The number of frames the attacker and the target pause when the attack hits',
+		'angle', 'The base direction of the knockback. It is automatically flipped based on the direction the player is facing. Opponents can DI away from this angle slightly',
+		'lifetime', 'The number of frames the hitbox is active',
+		'shape', 'The sprite used for collision checking. Use the enum "HITBOX_SHAPE.sprite" for custom hitbox shapes',
+		'group', 'The hitbox group that the new hitbox will be in. Each group can only hit an opponent once until the groups are reset',
+		'[flipper]', 'Optional. Any special effects applied to the knockback angle. Must be from the enum "FLIPPER"'
+	],
+	returns: 'Real (instance ID)',
+	example: {
+		code:
+`create_melee(10, 0, 0.6, 0.4, 5, 7, 0.3, 5, 80, 3, HITBOX_SHAPE.rectangle, 0);`,
+		description:`<p>Creates a melee hitbox 10 pixels in front of the player. The hitbox will hit opponents up into the air (80 degrees) with weak knockback.
+		It has only 5 frames of hitlag and lasts for 3 frame.s It doesn't have any special flippers applied.</p>`
+	}
+},
 
-test_function: {
-  description:
-    `<p>All instances have a unique identifier (<a href="#id">id</a>) which can be used to modify and manipulate them while a game is running, but you may not always know what the id for a specific instance is and so this function can help as you can use it to iterate through all of them to find what you need. You specify the object that you want to find the instance of and a number, and if there is an instance at that position in the instance list then the function returns the id of that instance, and if not it returns the special keyword <strong>noone</strong>. You can also use the keyword <strong>all</strong> to iterate through all the instances in a room, as well as a parent object to iterate through all the instances that are part of that parent / child hierarchy and you can even specify an instance itself (if you have its id) as a check to see if it actually exists in the current room. Please note that as instances are sorted in an <i>arbitrary</i> manner, there is no specific order to how the instances are checked by this function, and any instance can be in any position.</p>
-    <p>The maximum value for "n" in this function would be</p>
-    <ul>
-      <li>For the keyword <strong>all</strong>: <a href="#instance_count">instance_count - 1</a></li>
-      <li>For the object index: <a href="#instance_number">instance_number(OBJ) - 1</a></li>
-    </ul>`,
-  syntax: 'instance_find(obj, n);',
-  args: [
-    'obj', 'The object to find the nth instance of',
-    'n', 'The number of the instance to find'
-  ],
-  returns: 'Real (either an instance ID value or the keyword noone)',
-  example: {
-    code:
-`var i;
-for (i = 0; i < instance_number(obj_Enemy); i += 1)
+set_speed: {
+	description:
+		`<p>This function can be used to control the speed of a player instance. You can choose to change the speed relative to the current speed or to an absolute amount.</p>`,
+	syntax: 'set_speed(hsp, vsp, [hsp_relative], [vsp_relative]);',
+	args: [
+		'hsp', 'The change in horizontal speed',
+		'vsp', 'The change in vertical speed', 
+		'[hsp_relative]', 'Optional. Specify whether the change in horizontal speed is relative (true) or absolute (false). By default it is false',
+		'[vsp_relative]', 'Optional. Specify whether the change is vertical speed relative (true) or absolute (false). By default it is false',
+	],
+	returns: '---',
+	example: {
+		code:
+`//Increase the hsp by 2
+set_speed(2, 0, true, true);
+
+//Stop the player
+set_speed(0, 0);
+
+//Decrease vsp by 10 and stop all hsp
+set_speed(0, -10, false, true);`,
+		description:`<p>The above code shows multiple ways to use the function for controlling player speed.</p>`
+	}
+},
+
+change_facing: {
+	description:
+		`<p>Changes the direction the player is facing based on the control stick.</p>`,
+	syntax: 'change_facing([control_stick]);',
+	args: [
+		'[control_stick]', 'Optional. You can set it to check the Rstick instead of the Lstick, which is the default',
+	],
+	returns: '---',
+	example: {
+		code:
+`if (!on_ground(x,y))
     {
-     enemy[i] = instance_find(obj_Enemy,i);
+    change_facing();
     }`,
-    description: `<p>The above code will use a for loop to iterate through all the instances of "obj_Enemy" and store their id in the array "enemy[]".</p>`
-  }
+		description:`<p>The example code will make the player character face the direction of the <mark>Lstick</mark> while in the air.</p>`
+	}
+},
+
+stick_direction: {
+	description:
+		`<p>Checks if the specified control stick is tilted in the given direction.</p>`,
+	syntax: 'stick_direction(control_stick, direction);',
+	args: [
+		'control_stick', 'Either Lstick or Rstick',
+		'direction', 'The direction that will be checked. Must be from the DIR enum',
+	],
+	returns: 'Bool',
+	example: {
+		code:
+`if (stick_direction(Lstick, DIR.down))
+    {
+    attack_start(my_attacks[? "Dair"]);
+    }`,
+		description:`<p>The above code checks if the control stick is pointed downwards, and starts a Down Air attack if it is true.</p>`
+	}
+},
+
+attack_start: {
+	description:
+		`<p>This function starts whichever attack script you pass to it. The specific effects are as follows:</p>
+		<ul>
+			<li>The <mark>attack_script</mark> variable is set to the passed script reference</li>
+			<li>The player's state is set to the attacking state</li>
+			<li>The script is run with the special phase <mark>PHASE.start</mark></li>
+		</ul>`,
+	syntax: 'attack_start(script)',
+	args: [
+		'script', 'The script that will be run as an attack. (Note: Do not put () after the script as that will run the script instead of passing it into the function)'
+	],
+	returns: '---',
+	example: {
+		code:
+`if (button(INPUT.attack, buff))
+    {
+    attack_start(my_attacks[? "Jab"]);
+    }`,
+		description:`<p>If a button set to <mark>INPUT.attack</mark> has been pressed in the last few frames (determined by the macro <a href="#buff">buff</a>), the character will
+		start its Jab attack.</p>`
+	}
 }
 
 // end functions
--- macros.js
+++ macros.js
@@ -1,6 +1,33 @@
 var macros = [
 
-'macro_name', 'value', 'description',
-'macro_name', 'value', 'description',
+	'debug', 'Bool (false)', 'Turns on or off debug mode, a special mode that shows things like the input buffer, FPS, and state log',
+	'show_hitboxes', 'Bool (false)', 'Toggle the visibility of all hitbox objects',
+	'show_hurtboxes', 'Bool (false)', 'Toggle the visibility of all hurtbox objects',
+	'show_collision_boxes', 'Bool (false)', 'Toggle the visibility of the collision boxes of all players',
 
+	'max_players', 'Int (8)', 'The maximum number of players that the game allows',
+
+	'camera_width', 'Real (960)', 'The width of the camera',
+	'camera_height', 'Real (544)', 'The height of the camera',
+	'camera_boundary', 'Real (120)', 'The minimum distance of the camera from the edges of the room',
+	
+	'back_clear', 'Constant (-1)', 'The value that will be recognized as the clear layer in backgrounds',
+	
+	'profiles_save', 'String ("Player_Profiles.sav")', 'The filename of the profile save file',
+	
+	'number_of_devices', 'Constant (14)', 'The number of ports that are checked for gamepad connections',
+	
+	'stick_check_frames', 'Int (3)', `The number of frames back the stick values are stored for checking stick speed.
+	Higher numbers will detect higher stick speeds more easily.`,
+	
+	'Lstick', 'Constant (0)', 'A number that represents the Left stick for use in functions',
+	'Rstick', 'Constant (1)', 'A number that represents the Right stick for use in functions',
+	'stick_tilt_amount', 'Real (0.4)', 'The control stick threshold that must be passed to count as "tilting" the left stick',
+	'stick_flick_speed', 'Real (0.4)', 'The minimum speed for a left stick movement to count as a "flick"',
+	'stick_flick_amount', 'Read (0.7)', 'The control stick threshold that must be passed to count as "flicking" the left stick',
+	'stick_flick_buff', 'Int (6)', 'The number of frames back that the function "stick_flicked" checks for a flick input',
+	
+	'rstick_tilt_amount', 'Real (0.4)', 'The control stick threshold that must be passed to count as "tilting" the right stick',
+	'rstick_flick_speed', 'Real (0.4)', 'The minimum speed for a right stick movement to count as a "flick"',
+	'rstick_flick_amount', 'Read (0.7)', 'The control stick threshold that must be passed to count as "flicking" the right stick',
 ]
